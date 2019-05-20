d7719ee86aeb979bd3ebc13f8a59bf614de4b11c
Fereshte Nami <50325004+Ferishtnamida@users.noreply.github.com>
2019-05-20 14:21:22 +0200

    Update Recipes.php
    added grid

--- Deláhors-Projekt/assets/Pages/Recipes.php
+++ Deláhors-Projekt/assets/Pages/Recipes.php
@@ -10,13 +10,20 @@
             <br>
         </div>
 
+        <div class="grid-container">
+            <!-- stick pictures with links in here -->
+           <div class="grid-item">whores</div>
+           <div class="grid-item">ahaha</div>
+           <div class="grid-item">hehe</div>
+        </div>
+        
         <div>
             <!-- put in boxes that will link to the vego and vegan recipes -->
-            <bild><a href="recipes/Vegan.php"><img src="../img/pic1.png"></a></bild>
+            <!--<bild><a href="recipes/Vegan.php"><img src="../img/pic1.png"></a></bild>
             <h4>Vegan recipes</h4>
             <br>
             <bild><a href="recipes/Vegetarian.php"><img src="../img/pic2.png"></a></bild>
-            <h4>Vegetarian recipes</h4>
+            <h4>Vegetarian recipes</h4>-->
         </div>
 
     </body>
