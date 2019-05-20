a78c82d76fcf50f7c46a0fc4cb5efe376ac9ce08
Fereshte Nami <50325004+Ferishtnamida@users.noreply.github.com>
2019-05-20 13:40:23 +0200

    Add files via upload

---
+++ Deláhors-Projekt/assets/Pages/Register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <h1>Register</h1>
+        <br>
+    </head>
+    <body>
+        <div>
+            <form>
+                <input type="text" name="regUname" id="regUname">
+                <br>
+                <UserName>UserName</UserName>
+                <br>
+
+                <br>
+                <input type="text" name="regUemail" id="regUemail">
+                <br>
+                <UserEmail>Email</UserEmail>
+                <br>
+
+                <br>
+                <input type="password" name="regUpassword" id="regUpassword">
+                <br>
+                <UserPassword>Password</UserPassword>
+                <br>
+
+                <br>
+                <button id="regButton" type="sumbit" class="regButton">Register</button>
+            </form>
+        </div>
+
+        <div>
+            <hr>
+            <h4>Have an account?</h4>
+            <a href="Login.php">Log in here!</a>
+        </div>
+    </body>
+</html>
